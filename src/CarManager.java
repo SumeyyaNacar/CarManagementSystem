935d1c16638be327a1d0574aed32d5dfb0fff88e
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-02 00:10:38 +0300

    Update Car.java

--- src/CarManager.java
+++ src/CarManager.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CarManager {
+
+    private List<Car> car;//Araclari icinde tutacak bir liste
+    private Scanner scanner;//kullanicidan girdi almak icin Scanner nesnesi
+
+    //param.li const--yapici method
+
+
+    public CarManager() {
+        this.car =new ArrayList<>();//listeyi baslattik
+        this.scanner = new Scanner(System.in);//scanner nesnesi olusturduk
+    }
+    
 }
